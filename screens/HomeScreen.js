3f07b470eb2b967f6741fa16d73cfdef39e89ed3
Yurio Dwi LS <rea.riera@gmail.com>
2023-11-21 20:13:13 +0700

    add header components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 import { Ionicons } from "@expo/vector-icons";
+import Header from "../components/Header";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -17,6 +18,8 @@ const HomeScreen = () => {
       headerStyle: {
         backgroundColor: "#003580",
         height: 110,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
       },
       headerRight: () => (
         <Ionicons
@@ -30,7 +33,7 @@ const HomeScreen = () => {
   }, []);
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Header />
     </View>
   );
 };
