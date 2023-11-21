c6a449caf59c69d0522d4a112f1c71280b033a60
Yurio Dwi LS <rea.riera@gmail.com>
2023-11-21 22:39:16 +0700

    add destination, date, modals

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import { ModalPortal } from "react-native-modals";
 import StackNavigator from "./StackNavigator";
 
 export default function App() {
   return (
     <>
       <StackNavigator />
+      <ModalPortal />
     </>
   );
 }
