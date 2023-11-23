fc7c1d3d326f44d1cfb8fd6ab822dff08d727322
Baryova <baryova42@gmail.com>
2023-11-24 03:36:18 +0700

    Added Redux, BookingScreen, ConfirmationScreen, UserScreen

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { ModalPortal } from "react-native-modals";
 import StackNavigator from "./StackNavigator";
+import { Provider } from "react-redux";
+import store from "./Store"
+
 
 export default function App() {
   return (
     <>
+    <Provider store={store}>
       <StackNavigator />
       <ModalPortal />
+      </Provider>
     </>
   );
 }
