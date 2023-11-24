3169964a3b790237da1998897655305e914ad001
Yurio Dwi LS <rea.riera@gmail.com>
2023-11-24 23:33:00 +0700

    test merge branch

---
+++ components/Js.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Js = () => {
+  return (
+    <View>
+      <Text>Js</Text>
+    </View>
+  );
+};
+
+export default Js;
+
+const styles = StyleSheet.create({});
